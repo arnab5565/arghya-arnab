131f1bf0480fe3ef2bc727481b787da06b052fb3
arnab5565 <62198084+arnab5565@users.noreply.github.com>
2020-07-29 21:42:17 +0530

    Create arghya_arnab_ai.py

---
+++ arghya_arnab_ai.py
@@ -0,0 +1,5 @@
+#creating by arnab +++++++++++++++++++++++++++++++++++++++++++
+
+print("This is a AI and under the colllaboration")
+
+#this is arnab maiti section|||||||||||||||||||||||||||||||||||||||||
