0adbde0a85d6abc03106485ac3c0e5e05eac87ef
arnab5565 <62198084+arnab5565@users.noreply.github.com>
2020-07-30 16:14:01 +0530

    Update arghya_arnab_ai.py

--- arghya_arnab_ai.py
+++ arghya_arnab_ai.py
@@ -1,6 +1,13 @@
-#creating by arnab +++++++++++++++++++++++++++++++++++++++++++
-
+#==============================================AI Brain section by arnab=================================================== 
+#=============================================the project for creating a sentiment analyser================================
+from textblob import Textblob
 print("This is a AI and under the colllaboration")
+text=input("Write your feelings here ")
+obj=TextBlob(text)#creating a object for the sentiment
+#======================================SENTIMENT CALCULATION FOR THE OBJECT========================================
+sentiment=obj.sentiment.polarity 
+print(sentiment)
+#==============if the sentiment is negative its is sorrow and if positive then it is a good feeling=================
 
-#this is arnab maiti section|||||||||||||||||||||||||||||||||||||||||
-#this is below section is arghya's one
+#============================================this is arnab maiti section=============================================
+#=============================================this is below section is Arghya's one===================================
