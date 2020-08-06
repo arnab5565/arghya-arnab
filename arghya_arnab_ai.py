cd9a25555b54385ebc9be7c939f6a2fc0db04cc6
arnab5565 <62198084+arnab5565@users.noreply.github.com>
2020-08-06 22:11:05 +0530

    Update arghya_arnab_ai.py

--- arghya_arnab_ai.py
+++ arghya_arnab_ai.py
@@ -7,6 +7,12 @@ obj=TextBlob(text)#creating a object for the sentiment
 #======================================SENTIMENT CALCULATION FOR THE OBJECT========================================
 sentiment=obj.sentiment.polarity 
 print(sentiment)
+#now let us do it
+if(sentiment<0):
+  print('please dont be so much sad the life is like this nothing to do ')
+
+else:
+  print('Good i wish you that you are always cheerful in your life')
 #==============if the sentiment is negative its is sorrow and if positive then it is a good feeling=================
 
 #============================================this is arnab maiti section=============================================
